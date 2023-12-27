fb8ea02bd336cfc4e2d9b1207d3f41f52af5fdf5
Marcel Hofmann <hofman24@hm.edu>
2023-12-27 22:20:07 +0100

    Trying to get a SSD1306 display to work

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Raspberry Pi Pico W based CO2 Sensor
 The Pico W is used to measure the CO2 levels in a room. The value is displayed using a simple LCD display as well as sending the data via MQTT to an external MQTT Broker using the onboard WiFi Chip.
 
+> :warning: A MacOS device has been used as the dev machine, build and toolings might not work on other machines.
+
+## Functionality 
+
+- Print co2 measurements to SSD1306 OLED display. Connect SDA to GPIO1 and SCL to GPIO2, provide 3.3V power.
+- Send co2 measurements via MQTT to external broker / server
+
 ## Build 
 
 Execute the following steps to build the project:
@@ -14,4 +21,12 @@ make
 
 # Optionally if you are using picotool execute this to flash the pico:
 picotool load -f blink.uf2
+```
+
+## Console Prints via minicom
+
+To see the output of printf on the dev machine install minicom on your device `brew install minicom` and then execute this command: 
+
+```
+minicom -b 115200 -o -D /dev/cu.usbmodem1101
 ```
--- picow_blink.c
+++ picow_blink.c
@@ -1,22 +1,46 @@
-/**
- * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+#include "ssd1306.h"
 
 int main() {
   stdio_init_all();
   if (cyw43_arch_init()) {
-    printf("Wi-Fi init failed");
+    printf("Wi-Fi init failed\n");
     return -1;
   }
-  while (true) {
+
+  // Setup Display
+  printf("Main: Waiting for serial port to be ready...\n");
+  sleep_ms(5000);
+
+  printf("Test!\n");
+
+  init_i2c();
+  i2c_scan();
+
+  oled_init();
+
+  uint8_t invert = 0;
+  while (1) {
+    printf("Flashing display \n");
+    flash_display(invert);
+    if (invert == 0) {
+      invert = 1;
+    } else {
+      invert = 0;
+    }
+
+    sleep_ms(500);
+  }
+
+  while (1) {
+    printf("Updating display...\n");
+
+    oled_string("Hello, World!");
+
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     sleep_ms(250);
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-    sleep_ms(250);
+    sleep_ms(4750);
   }
 }
