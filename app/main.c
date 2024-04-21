2cc02da23c177cb87a3d57fd44f6186b45d10be9
Marcel Hofmann <hofman24@hm.edu>
2024-04-21 21:22:45 +0200

    tcp client

--- app/main.c
+++ app/main.c
@@ -1,58 +1,27 @@
+#include "main.h"
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <task.h>
 
 #include "i2c/i2c.h"
-#include "pico/cyw43_arch.h"
-#include "pico/stdlib.h"
-#include "scd40/scd40.h"
+#include "projdefs.h"
 #include "ssd1306/display.h"
 #include "ssd1306/ssd1306.h"
+#include "tasks/network_task.h"
 #include "tasks/read_data_task.h"
 #include "tasks/update_display_task.h"
 
-volatile QueueHandle_t queue = NULL;
+volatile QueueHandle_t display_queue = NULL;
+volatile QueueHandle_t network_queue = NULL;
 TaskHandle_t read_data_handle = NULL;
 TaskHandle_t update_display_handle = NULL;
-
-void init_wifi(const char* ssid, const char* password) {
-  if (cyw43_arch_init()) {
-    printf("Wi-Fi init failed\n");
-    while (1)
-      ;
-  }
-
-  printf("init_wifi: Connecting to wifi...\n");
-
-  int status =
-      cyw43_arch_wifi_connect_blocking(ssid, password, CYW43_AUTH_WPA2_AES_PSK);
-
-  if (status != 0) {
-    printf("init_wifi: Wi-Fi Connection failed: %d \n", status);
-    // while (1)
-    //;
-  }
-
-  printf("init_wifi: Successfully connected!\n");
-}
+TaskHandle_t network_task_handle = NULL;
 
 int main() {
   stdio_init_all();
 
-  sleep_ms(2000);  // Wait for serial_port to be initialized
-
-  const char* ssid = WIFI_SSID;
-  const char* password = WIFI_PASSWORD;
-
-  if (!ssid || !password) {
-    printf("main: SSID or Wi-Fi password not provided!\n");
-    return -1;
-  }
-
-  init_wifi(ssid, password);
+  sleep_ms(2000); // Wait for serial_port to be initialized
 
   // Init display
   init_i2c();
@@ -79,16 +48,22 @@ int main() {
 
   BaseType_t update_display_status =
       xTaskCreate(update_display_task, "UPDATE_DISPLAY_TASK", 4112,
-                  (void*)&ud_params, 1, &update_display_handle);
+                  (void *)&ud_params, 1, &update_display_handle);
 
-  printf("main: Creating queue\n");
-  queue = xQueueCreate(5, sizeof(int));
+  // Create network task
+  BaseType_t network_task_status = xTaskCreate(
+      network_task, "NETWORK_TASK", 2056, NULL, 1, &network_task_handle);
 
-  if (read_data_status == pdPASS && update_display_status == pdPASS) {
+  printf("main: Creating queues\n");
+  display_queue = xQueueCreate(5, sizeof(int));
+  network_queue = xQueueCreate(5, sizeof(measurements_t));
+
+  if (read_data_status == pdPASS && update_display_status == pdPASS &&
+      network_task_status == pdPASS) {
     printf("main: Starting scheduler!\n");
     vTaskStartScheduler();
   } else {
-    printf("main: Unable to start scheduler! RD: %s UD: %s", read_data_status,
+    printf("main: Unable to start scheduler! RD: %ld UD: %ld", read_data_status,
            update_display_status);
     return -1;
   }
--- app/main.h
+++ app/main.h
@@ -1,4 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#endif  // MAIN_H
+typedef struct measurements_t {
+  int co2;
+  int temp;
+  int humidity;
+} measurements_t;
+
+#endif // MAIN_H
