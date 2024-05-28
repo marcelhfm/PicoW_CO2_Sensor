45bd6d0aaf3dc558144d2be3441b30746e7e9e3d
Marcel Hofmann <hofman24@hm.edu>
2024-05-28 20:14:13 +0200

    feat: added workflow

--- app/main.c
+++ app/main.c
@@ -23,13 +23,13 @@ TaskHandle_t network_task_handle = NULL;
 int main() {
   stdio_init_all();
 
+  sleep_ms(2000); // Wait for serial_port to be initialized
+
   if (watchdog_caused_reboot()) {
     printf("Rebooted by Watchdog!\n");
   } else {
     printf("Clean boot\n");
   }
-
-  sleep_ms(2000); // Wait for serial_port to be initialized
 
   // Init display
   init_i2c();
